d46c0bc2d0d64bac13f9702fec9858e1ce3999f4
CaterpillarSan <anna.isomae@gmail.com>
2019-01-25 17:29:26 +0900

    コメントつけたり

--- lsb.go
+++ lsb.go
@@ -12,45 +12,53 @@ import (
 )
 
 const BUFSIZE = 2048
+const INPUT_TEXTFILE = "input.txt"
+const OUTPUT_FILEHEAD = "stgn_"
 
 func main() {
-	var toStegano bool
-	var filename string
-	var secretText string
 
-	flag.BoolVar(&toStegano, "enc", false, "True: encrypt, False: decrypt")
+	var (
+		toStegano bool   // true : encryption / false : decription
+		filename  string // 使用するファイル名
+	)
+
+	flag.BoolVar(&toStegano, "enc", false, "true: encrypt, false: decrypt")
 	flag.StringVar(&filename, "file", "example.png", "Image filename")
-	flag.StringVar(&secretText, "text", "This is sample secret text.", "Text you wanto hide")
 
 	flag.Parse()
 
+	// @Note 該当ファイルはmainと同じディレクトリに置く
 	img := inputImage(filename)
 
 	if toStegano {
-		encrypt(img)
+		// Encryption
+		encImg := encrypt(img)
+		outputImage(filename, encImg)
 		fmt.Println("==== Encryption OK ====")
 	} else {
+		// Decription
 		text := decrypt(img)
 		fmt.Println("==== Decription ====\n" + text)
 	}
 }
 
+// mainと同じディレクトリにある INPUT_TEXTFILE の文字列をBUFSIZEぶん読み出す
 func inputTextBuf() []byte {
-	file, err := os.Open(`input.txt`)
+	file, err := os.Open(INPUT_TEXTFILE)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
 	buf := make([]byte, BUFSIZE)
-	_, err = file.Read(buf)
-	if err != nil {
+	if _, err = file.Read(buf); err != nil {
 		log.Fatal(err)
 	}
 
 	return buf
 }
 
+// filenameで指定されたpngファイルを読み出す
 func inputImage(filename string) *image.RGBA {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -65,27 +73,30 @@ func inputImage(filename string) *image.RGBA {
 	return imageToRGBA(img)
 }
 
-func outputImage(img *image.RGBA) {
+// img構造体をファイルに書き出す
+func outputImage(inputFilename string, img *image.RGBA) {
 	// 書き出し用ファイル準備
-	outfile, _ := os.Create("out.png")
+	outfile, _ := os.Create(OUTPUT_FILEHEAD + inputFilename)
 	defer outfile.Close()
-	// 書き出し
 
+	// 書き出し
+	// 圧縮入るとまずそうなので念のため
 	var encoder = &png.Encoder{CompressionLevel: -1}
 	encoder.Encode(outfile, img)
-
 }
 
-func encrypt(img *image.RGBA) {
+// 画像に文字を入れ込む
+func encrypt(img *image.RGBA) *image.RGBA {
 	// pixel := img.Pix
 	dx, dy := img.Bounds().Dx(), img.Bounds().Dy()
 	vec := inputTextBuf()
 	tsize := len(vec)
 
-	// 文字列が長すぎない前提
-
+	// 文字列に対して画像が小さすぎない前提
 	for i := 0; i < tsize*2; i++ {
 		c := vec[i/2]
+
+		// @todo 正方形だと死ぬ
 		xx := i % dx
 		yy := i % dy
 		rr, gg, bb, aa := img.At(xx, yy).RGBA()
@@ -104,10 +115,10 @@ func encrypt(img *image.RGBA) {
 		img.SetRGBA(xx, yy, color.RGBA{r, g, b, a})
 	}
 
-	// ファイル保存
-	outputImage(img)
+	return img
 }
 
+// 画像から文字列取得
 func decrypt(img *image.RGBA) string {
 	var vec [BUFSIZE]byte // めんどいので文字数固定
 	dx, dy := img.Bounds().Dx(), img.Bounds().Dy()
